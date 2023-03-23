27897da5c235c97003884c00f4effa47665b36c0
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-23 23:01:32 +0100

    GlobalStyle added

---
+++ gnnews/src/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    background: #ccc;
+    min-height: 80vh;
+    margin: 0 auto;
+    color: #ccc;
+  }
+`;
