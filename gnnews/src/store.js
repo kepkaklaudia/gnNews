79c5d5af0e9340c788f733ecbddf78ab833796fb
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 12:00:31 +0100

    store added

--- gnnews/src/store.js
+++ gnnews/src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import newsReducer from "./getData/newsSlice";
+import createSagaMiddleware from "redux-saga";
+import { newsSaga } from "./getData/newsSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
+    news: newsReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(newsSaga);
 
 export default store;
