c8dc44f14ce9dc24e571b467ca6fd0faf55d92a0
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 09:13:27 +0100

    colors added

--- gnnews/src/theme.js
+++ gnnews/src/theme.js
@@ -2,5 +2,10 @@ export const theme = {
   colors: {
     mercury: "#F5F5FA",
     grey: "#3a424d",
+    silver: "#cdcdcd",
+    lemon: "#a3cf34",
+    white: "#FFF",
+    mystic: "#E4E6F0",
+    transparent: "rgba(100, 100, 100, 0)",
   }
 };
