159a0382d201e87ac83a8383af3120940a286c45
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 10:36:31 +0100

    onClick added

--- gnnews/src/Header/Menu/Countries/index.js
+++ gnnews/src/Header/Menu/Countries/index.js
@@ -2,12 +2,13 @@ import { nanoid } from "@reduxjs/toolkit";
 import { countriesList } from "./countriesList";
 import { Image, Wrapper, StyledLink, Name } from "./styled";
 
-export const Countries = () => (
+export const Countries = ({ onClick }) => (
   <>
     {countriesList.map((country) =>
       <StyledLink
         to={`/country/${country.short}`}
         key={nanoid()}
+        onClick={onClick}
       >
         <Wrapper>
           <Image src={country.flag} alt="flag" />
