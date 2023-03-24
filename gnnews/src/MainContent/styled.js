45fdf00045b98300171a735326a6f590c91fc1eb
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 11:48:52 +0100

    Wrapper added

--- gnnews/src/MainContent/styled.js
+++ gnnews/src/MainContent/styled.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+export const Wrapper = styled.div`
+  margin: 150px 40px 100px 40px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  justify-items: center;
+`;
+
 export const StyledLink = styled(Link)`
   text-decoration: none;
   color: ${({ theme }) => theme.colors.grey};
