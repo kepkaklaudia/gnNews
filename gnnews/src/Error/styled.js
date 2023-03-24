593cd14b2a66f5facd7bb737805cadbd264854af
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 15:59:51 +0100

    Wrapper added

---
+++ gnnews/src/Error/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 250px auto 0;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  text-align: center;
+`;
