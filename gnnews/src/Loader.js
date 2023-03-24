7973f6129d4e943e47b9247aa689597e814b3d83
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 14:26:22 +0100

    media added

--- gnnews/src/Loader.js
+++ gnnews/src/Loader.js
@@ -11,38 +11,38 @@ export const Loader = styled.div`
   box-sizing: border-box;
   animation: rotation 1s linear infinite;
 
-&::after {
-  content: '';  
-  box-sizing: border-box;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  border-left: 7px solid ${({ theme }) => theme.colors.lemon};
-  border-bottom: 7px solid transparent;
-  animation: rotation 0.5s linear infinite reverse;
-}
-
-@keyframes rotation {
-  0% {
-    transform: rotate(0deg);
-  }
-  
-  100% {
-    transform: rotate(360deg);
-  }
-} 
-
-@media (max-width: 700px) {
-  width: 70px;
-  height: 70px;
-  margin-top: 200px;
-
-  &::after {
+  @media (max-width: 700px) {
     width: 70px;
     height: 70px;
+    margin-top: 200px;
   }
-}
+
+  &::after {
+    content: '';
+    box-sizing: border-box;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border-left: 7px solid ${({ theme }) => theme.colors.lemon};
+    border-bottom: 7px solid transparent;
+    animation: rotation 0.5s linear infinite reverse;
+
+    @media (max-width: 700px) {
+      width: 70px;
+      height: 70px;
+    }
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+  
+    100% {
+      transform: rotate(360deg);
+    }
+  } 
 `;
