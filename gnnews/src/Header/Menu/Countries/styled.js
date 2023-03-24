efdb33b5694fd73a53179fdbd48d192708d3619f
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 10:31:15 +0100

    Wrapper added

--- gnnews/src/Header/Menu/Countries/styled.js
+++ gnnews/src/Header/Menu/Countries/styled.js
@@ -10,6 +10,17 @@ export const StyledLink = styled(Link)`
   }
 `;
 
+export const Wrapper = styled.div`
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 1px 1px 10px 0px ${({ theme }) => theme.colors.silver};
+  display: flex;
+  gap: 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+  padding: 15px 10px;
+  width: 200px;
+`;
+
 export const Image = styled.img`
   width: 40px;
 
