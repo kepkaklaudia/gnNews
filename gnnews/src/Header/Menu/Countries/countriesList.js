769d60658c035306f8e6d65ff9a51e7b332df762
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 10:28:04 +0100

    countriesList added

---
+++ gnnews/src/Header/Menu/Countries/countriesList.js
@@ -0,0 +1,274 @@
+const flagURL = "https://newsapi.org/images/flags/"
+
+export const countriesList = [
+	{
+		name: "Argentina",
+		flag: `${flagURL}ar.svg`,
+		short: "ar",
+	},
+	{
+		name: "Australia",
+		flag: `${flagURL}au.svg`,
+		short: "au",
+	},
+	{
+		name: "Austria",
+		flag: `${flagURL}at.svg`,
+		short: "at",
+	},
+	{
+		name: "Belgium",
+		flag: `${flagURL}be.svg`,
+		short: "be",
+	},
+	{
+		name: "Brazil",
+		flag: `${flagURL}br.svg`,
+		short: "br",
+	},
+	{
+		name: "Bulgaria",
+		flag: `${flagURL}bg.svg`,
+		short: "bg",
+	},
+	{
+		name: "Canada",
+		flag: `${flagURL}ca.svg`,
+		short: "ca",
+	},
+	{
+		name: "China",
+		flag: `${flagURL}cn.svg`,
+		short: "cn",
+	},
+	{
+		name: "Colombia",
+		flag: `${flagURL}co.svg`,
+		short: "co",
+	},
+	{
+		name: "Cuba",
+		flag: `${flagURL}cu.svg`,
+		short: "cu",
+	},
+	{
+		name: "Czech Republic",
+		flag: `${flagURL}cz.svg`,
+		short: "cz",
+	},
+	{
+		name: "Egypt",
+		flag: `${flagURL}eg.svg`,
+		short: "eg",
+	},
+	{
+		name: "France",
+		flag: `${flagURL}fr.svg`,
+		short: "fr",
+	},
+	{
+		name: "Germany",
+		flag: `${flagURL}de.svg`,
+		short: "de",
+	},
+	{
+		name: "Greece",
+		flag: `${flagURL}gr.svg`,
+		short: "gr",
+	},
+	{
+		name: "Hong Kong",
+		flag: `${flagURL}hk.svg`,
+		short: "hk",
+	},
+	{
+		name: "Hungary",
+		flag: `${flagURL}hu.svg`,
+		short: "hu",
+	},
+	{
+		name: "India",
+		flag: `${flagURL}in.svg`,
+		short: "in",
+	},
+	{
+		name: "Indonesia",
+		flag: `${flagURL}id.svg`,
+		short: "id",
+	},
+	{
+		name: "Ireland",
+		flag: `${flagURL}ie.svg`,
+		short: "ie",
+	},
+	{
+		name: "Israel",
+		flag: `${flagURL}il.svg`,
+		short: "il",
+	},
+	{
+		name: "Italy",
+		flag: `${flagURL}it.svg`,
+		short: "it",
+	},
+	{
+		name: "Japan",
+		flag: `${flagURL}jp.svg`,
+		short: "jp",
+	},
+	{
+		name: "Latvia",
+		flag: `${flagURL}lv.svg`,
+		short: "lv",
+	},
+	{
+		name: "Lithuania",
+		flag: `${flagURL}lt.svg`,
+		short: "lt",
+	},
+	{
+		name: "Malaysia",
+		flag: `${flagURL}my.svg`,
+		short: "my",
+	},
+	{
+		name: "Mexico",
+		flag: `${flagURL}mx.svg`,
+		short: "mx",
+	},
+	{
+		name: "Morocco",
+		flag: `${flagURL}ma.svg`,
+		short: "ma",
+	},
+	{
+		name: "Netherlands",
+		flag: `${flagURL}nl.svg`,
+		short: "nl",
+	},
+	{
+		name: "New Zealand",
+		flag: `${flagURL}nz.svg`,
+		short: "nz",
+	},
+	{
+		name: "Nigeria",
+		flag: `${flagURL}ng.svg`,
+		short: "ng",
+	},
+	{
+		name: "Norway",
+		flag: `${flagURL}no.svg`,
+		short: "no",
+	},
+	{
+		name: "Philippines",
+		flag: `${flagURL}ph.svg`,
+		short: "ph",
+	},
+	{
+		name: "Poland",
+		flag: `${flagURL}pl.svg`,
+		short: "pl",
+	},
+	{
+		name: "Portugal",
+		flag: `${flagURL}pt.svg`,
+		short: "pt",
+	},
+	{
+		name: "Romania",
+		flag: `${flagURL}ro.svg`,
+		short: "ro",
+	},
+	{
+		name: "Russia",
+		flag: `${flagURL}ru.svg`,
+		short: "ru",
+	},
+	{
+		name: "Saudi Arabia",
+		flag: `${flagURL}sa.svg`,
+		short: "sa",
+	},
+	{
+		name: "Serbia",
+		flag: `${flagURL}rs.svg`,
+		short: "rs",
+	},
+	{
+		name: "Singapore",
+		flag: `${flagURL}sg.svg`,
+		short: "sg",
+	},
+	{
+		name: "Slovakia",
+		flag: `${flagURL}sk.svg`,
+		short: "sk",
+	},
+	{
+		name: "Slovenia",
+		flag: `${flagURL}si.svg`,
+		short: "si",
+	},
+	{
+		name: "South Africa",
+		flag: `${flagURL}za.svg`,
+		short: "za",
+	},
+	{
+		name: "South Korea",
+		flag: `${flagURL}kr.svg`,
+		short: "kr",
+	},
+	{
+		name: "Sweden",
+		flag: `${flagURL}se.svg`,
+		short: "se",
+	},
+	{
+		name: "Switzerland",
+		flag: `${flagURL}ch.svg`,
+		short: "ch",
+	},
+	{
+		name: "Taiwan",
+		flag: `${flagURL}tw.svg`,
+		short: "tw",
+	},
+	{
+		name: "Thailand",
+		flag: `${flagURL}th.svg`,
+		short: "th",
+	},
+	{
+		name: "Turkey",
+		flag: `${flagURL}tr.svg`,
+		short: "tr",
+	},
+	{
+		name: "UAE",
+		flag: `${flagURL}ae.svg`,
+		short: "ae",
+	},
+	{
+		name: "Ukraine",
+		flag: `${flagURL}ua.svg`,
+		short: "ua",
+	},
+	{
+		name: "United Kingdom",
+		flag: `${flagURL}gb.svg`,
+		short: "gb",
+	},
+	{
+		name: "United States",
+		flag: `${flagURL}us.svg`,
+		short: "us",
+	},
+	{
+		name: "Venuzuela",
+		flag: `${flagURL}ve.svg`,
+		short: "ve",
+	},
+];
