1c6006293389b5cd2fca6707c2e9b7136764a098
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 09:39:27 +0100

    Logo added

--- gnnews/src/Header/styled.js
+++ gnnews/src/Header/styled.js
@@ -4,3 +4,13 @@ import { NavLink } from "react-router-dom";
 export const NavigationLink = styled(NavLink)`
   text-decoration: none;
 `;
+
+export const Logo = styled.img`
+  cursor: pointer;
+  height: 90px;
+  margin-left: 20px;
+
+  @media (max-width: 700px) {
+    height: 50px;
+  }
+`;
