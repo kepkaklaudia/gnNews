32699b7a732937e94e8f5fcea0743cd98236afe9
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 09:48:28 +0100

    Container added

--- gnnews/src/Header/styled.js
+++ gnnews/src/Header/styled.js
@@ -1,6 +1,28 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.grey};
+  display: flex;
+  position: fixed;
+  top: 0;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  min-width: 280px;
+  height: 150px;
+  padding: 10px 40px;
+  z-index: 1;
+ 
+  @media (max-width: 768px) {
+    display: grid;
+    grid-tempate-column: 1fr;
+    height: 135px;
+    justify-content: center;
+    padding: 10px 10px;
+  }
+`;
+
 export const NavigationLink = styled(NavLink)`
   text-decoration: none;
 `;
