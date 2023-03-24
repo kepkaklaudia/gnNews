7e7cefa48c16abd3088c1e33d84ca342712b1248
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 10:06:16 +0100

    Routes added

--- gnnews/src/App.js
+++ gnnews/src/App.js
@@ -1,20 +1,30 @@
+import { Header } from './Header';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Footer } from './Footer';
 
 function App() {
   return (
     <>
-      <ThemeProvider
-        theme={theme}
-      >
-        <GlobalStyle />
-        <Footer />
-      </ThemeProvider>
+      <HashRouter>
+        <ThemeProvider
+          theme={theme}
+        >
+          <GlobalStyle />
+          <Routes>
+            <Route
+              path="/"
+              element={<>sth</>}
+            />
+          </Routes>
+          <Header />
+          <Footer />
+        </ThemeProvider>
+      </HashRouter>
     </>
   );
 }
 
 export default App;
-
