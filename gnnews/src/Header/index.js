3e952f97973bd3016e84153a7241203afdce6294
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 10:13:17 +0100

    Container added

--- gnnews/src/Header/index.js
+++ gnnews/src/Header/index.js
@@ -1,13 +1,13 @@
-import { Logo, NavigationLink } from './styled';
+import { Container, Logo, NavigationLink } from './styled';
 import { Menu } from './Menu';
 
 export const Header = () => (
-  <>
+  <Container>
     <Menu />
     <NavigationLink
       to="/"
     >
       <Logo src={""} alt="logo" />
     </NavigationLink>
-  </>
+  </Container>
 )
