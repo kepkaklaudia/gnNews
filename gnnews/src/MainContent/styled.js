5702d2edffe37e1f249a586b2b31e434379a9114
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 11:56:39 +0100

    Text added

--- gnnews/src/MainContent/styled.js
+++ gnnews/src/MainContent/styled.js
@@ -64,3 +64,17 @@ export const Info = styled.div`
     align-self: flex-end;
   `}
 `;
+
+export const Text = styled.p`
+  margin: 0;
+  font-size: 16px;
+  word-break: break-word;
+
+  @media (max-width: 700px) {
+    font-size: 12px;
+  }
+
+  @media (max-width: 500px) {
+    font-size: 10px;
+  }
+`;
