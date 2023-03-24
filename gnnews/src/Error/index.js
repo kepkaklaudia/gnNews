0e328b09f41950e36a501353778e12015ac5261d
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-24 14:14:23 +0100

    Error added

---
+++ gnnews/src/Error/index.js
@@ -0,0 +1,11 @@
+import { Image, Wrapper, Text } from "./styled";
+
+export const Error = () => {
+  return (
+    <Wrapper>
+      <Image />
+      <Text> Opps something went wrong
+      </Text>
+    </Wrapper>
+  )
+}
