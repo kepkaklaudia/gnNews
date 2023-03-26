91198bc05f4717ed55ff0ecdeb94cf79b91f8f0a
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-26 17:27:53 +0200

    store, App paths changed

--- gnnews/src/index.js
+++ gnnews/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store';
 import { Provider } from "react-redux";
+import store from './core/store';
+import App from './core/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
