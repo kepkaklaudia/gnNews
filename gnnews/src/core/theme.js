c18c19b6a0d7c14d65aa599ae5738450ed6f1347
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-26 17:28:38 +0200

    theme moved to core

---
+++ gnnews/src/core/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    mercury: "#F5F5FA",
+    grey: "#3A424D",
+    silver: "#CDCDCD",
+    lemon: "#A3CF34",
+    white: "#FFFFFF",
+    mystic: "#E4E6F0",
+    transparent: "#64646400",
+  }
+};
