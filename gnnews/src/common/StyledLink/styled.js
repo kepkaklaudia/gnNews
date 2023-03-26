7a9ad0905036c63dd241c76fbad747001622336f
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-26 17:32:16 +0200

    StyledLink moved to common

---
+++ gnnews/src/common/StyledLink/styled.js
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.grey};
+  
+  ${({ main }) => main && css`
+  width: 100%;
+  `}
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.lemon};
+  }
+`;
