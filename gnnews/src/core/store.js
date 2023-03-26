99c1b09800e424a21acf3ad569a8156b7c844771
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-03-26 17:28:53 +0200

    store moved to core

--- gnnews/src/core/store.js
+++ gnnews/src/core/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import newsReducer from "./getData/newsSlice";
-import createSagaMiddleware from "redux-saga";
 import { newsSaga } from "./getData/newsSaga";
-
+import createSagaMiddleware from "redux-saga";
+import newsReducer from "./getData/newsSlice";
+import viewReducer from "../features/Header/Icons/viewSlice"
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     news: newsReducer,
+    view: viewReducer,
   },
   middleware: [sagaMiddleware],
 });
