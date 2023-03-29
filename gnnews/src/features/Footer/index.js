ac0dcae2412ae4299de494ccaf8260b39eb6fe01
kepkaklaudia <kepkaklaudia@gmail.com>
2023-03-29 22:33:48 +0200

    status added

--- gnnews/src/features/Footer/index.js
+++ gnnews/src/features/Footer/index.js
@@ -2,9 +2,10 @@ import { useTranslation } from "react-i18next";
 import { useSelector } from 'react-redux';
 import { Container, Text } from './styled';
 import { Clock } from './Clock';
-import { selectNews } from '../../core/getData/newsSlice';
+import { selectNews, selectNewsStatus } from '../../core/getData/newsSlice';
 
 export const Footer = () => {
+  const status = useSelector(selectNewsStatus);
   const { articles } = useSelector(selectNews);
   const { t } = useTranslation();
 
@@ -13,10 +14,12 @@ export const Footer = () => {
       <Text>
         <Clock />
       </Text>
-      <Text>
-        {t("articlesNumber")}
-        {articles && articles.length}
-      </Text>
+      {status === "error" ? "" :
+        <Text>
+          {t("articlesNumber")}
+          {articles && articles.length}
+        </Text>
+      }
     </Container>
   )
 };
